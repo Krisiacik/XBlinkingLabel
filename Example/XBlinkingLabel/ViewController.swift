a8685a777895b29301d1580267d5c03b571a08ac
Kristian Angyal <kristian.angyal@starlingbank.com>
2021-01-24 03:03:32 +0000

    Added code.

--- Example/XBlinkingLabel/ViewController.swift
+++ Example/XBlinkingLabel/ViewController.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import XBlinkingLabel
 
 class ViewController: UIViewController {
 
+    var isBlinking = false
+    let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Setup the BlinkingLabel
+        blinkingLabel.text = "I blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+
+        // Create a UIButton to toggle the blinking
+        let toggleButton = UIButton(frame: CGRect(x: 10, y: 60, width: 125, height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: UIControl.State())
+        toggleButton.setTitleColor(UIColor.red, for: UIControl.State())
+        toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
+        view.addSubview(toggleButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func toggleBlinking() {
+        if (isBlinking) {
+            blinkingLabel.stopBlinking()
+        } else {
+            blinkingLabel.startBlinking()
+        }
+        isBlinking = !isBlinking
     }
-
 }
-
